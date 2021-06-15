3416517242ddbe6571b69b24a40b8cd6fd5778a8
koryun23 <koryun.grigoryan.5@gmail.com>
2021-06-15 16:20:09 +0400

    improving the design of sign up page

--- main.py
+++ main.py
@@ -392,6 +392,45 @@ class SignUpScreen(Screen):
         if pword1 == pword2:
             if database.insert(uname, pword1, email):
                 self.manager.current = 'sign_up_success'
+                self.ids.username.canvas.after.clear()
+                self.ids.email.canvas.after.clear()
+                self.ids.password.canvas.after.clear()
+                self.ids.retyped_password.canvas.after.clear()
+                with self.ids.username.canvas.after:
+                    Color(22/255,57/255,109/255,1)
+                    Rectangle(pos=self.ids.username.pos, size=(self.ids.username.width, 1))
+                with self.ids.password.canvas.after:
+                    Color(22/255,57/255,109/255,1)
+                    Rectangle(pos=self.ids.password.pos, size=(self.ids.password.width, 1))
+                with self.ids.email.canvas.after:
+                    Color(22/255,57/255,109/255,1)
+                    Rectangle(pos=self.ids.email.pos, size=(self.ids.email.width, 1))
+                with self.ids.retyped_password.canvas.after:
+                    Color(22/255,57/255,109/255,1)
+                    Rectangle(pos=self.ids.retyped_password.pos, size=(self.ids.retyped_password.width,1))
+                self.ids.username.text=""
+                self.ids.password.text=""
+                self.ids.retyped_password.text=""
+                self.ids.email.text = ""
+                return True
+        self.ids.username.canvas.after.clear()
+        self.ids.email.canvas.after.clear()
+        self.ids.password.canvas.after.clear()
+        self.ids.retyped_password.canvas.after.clear()
+        with self.ids.username.canvas.after:
+            Color(1,0,0,1)
+            Rectangle(pos=self.ids.username.pos, size=(self.ids.username.width, 1))
+        with self.ids.password.canvas.after:
+            Color(1, 0,0,1)
+            Rectangle(pos=self.ids.password.pos, size=(self.ids.password.width, 1))
+        with self.ids.email.canvas.after:
+            Color(1, 0,0,1)
+            Rectangle(pos=self.ids.email.pos, size=(self.ids.email.width, 1))
+        with self.ids.retyped_password.canvas.after:
+            Color(1, 0, 0,1)
+            Rectangle(pos=self.ids.retyped_password.pos, size=(self.ids.retyped_password.width,1))
+        
+        
     def go_to_login_page(self):
         self.manager.current="login_screen"
 class SignUpSuccess(Screen):
